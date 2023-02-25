e848c679dbec47d501834882d0358ddeaf6c7131
AllanKirui <nallawilljr@gmail.com>
2023-02-25 12:29:23 +0300

    Add logic to copy url link

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
+// calculate the window's scroll position to hide or show the 'back-to-top' button
 function calcScrollPos() {
   const backToTopEl = document.getElementById("back-to-top");
   let scrollTop = document.documentElement.scrollTop;
@@ -9,4 +10,27 @@ function calcScrollPos() {
   }
 }
 
+const copyBtn = document.getElementById("copy-btn");
+
+// add 'copy url' functionality to copy button on the sharing links
+async function copyUrlHandler() {
+  // if clipboard is available, write to it
+  if (navigator.clipboard) {
+    try {
+      await navigator.clipboard.writeText(`${window.location.href}`);
+      copyBtn.classList.add("copied");
+
+      setTimeout(() => {
+        copyBtn.classList.remove("copied");
+      }, 3000);
+    } catch (error) {
+      alert("Please copy URL address manually.");
+    }
+  } else {
+    // otherwise alert telling the user to copy manually
+    alert("Please copy URL address manually.");
+  }
+}
+
 window.addEventListener("scroll", calcScrollPos);
+copyBtn.addEventListener("click", copyUrlHandler);
